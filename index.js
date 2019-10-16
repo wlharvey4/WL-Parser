cabb47385c169240dcb2fe43f603f55cc12e586b
Wesley Harvey <pinecone062@gmail.com>
2019-10-16 09:18:04 -0700

    branch:prod

---
+++ index.js
@@ -0,0 +1,8 @@
+/* WLParser/index.js */
+
+module.exports = {
+    WLRecord: require('./lib/wlrecord'),
+    WLReader: require('./lib/wlreader'),
+    WLType:   require('./lib/wltype'),
+    WLChecks: require('./lib/wlchecks')
+};
